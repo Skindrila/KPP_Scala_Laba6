7b12e81f570d48455a7ba380437397e5f581b71b
Skindrila <skindrila_tv@mail.ru>
2018-05-26 04:26:42 +0300

    work

--- src/main/scala/lab/Main.scala
+++ src/main/scala/lab/Main.scala
@@ -7,17 +7,19 @@ object Main {
     val result = mutable.MutableList[Int]()
     for (i <- 0 until list.length) {
       if (list.indexOf(i) % 2 == 0)
-        list(i) * 10;
+        list(i) * 10
     }
     result
   }
-
+  
   def stringToInt(list: List[String]): List[Int] = {
-    list.map(_.toString.toInt)
-    /**list.foldLeft(0)(_ + _.toInt) */
+    list.foldLeft(List[Int]()){
+      (result: List[Int], stringEl: String) =>
+        result ::: List(stringEl.toInt)
+    }
   }
 
   def split(list: List[String]): List[Char] = {
-      list.flatMap(_.toString).toList
-    }
+    list.flatMap(_.toString).toList
+  }
 }
